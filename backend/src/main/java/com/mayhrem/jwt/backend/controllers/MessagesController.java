6b50ee296d61e10365eac8c6a277b49c8b19b489
Rene Cruz <renecruz.0711@gmail.com>
2024-03-26 00:00:31 -0600

    Adding backend project

---
+++ backend/src/main/java/com/mayhrem/jwt/backend/controllers/MessagesController.java
@@ -0,0 +1,22 @@
+package com.mayhrem.jwt.backend.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+// !This ia a controller and we're going to use the notation
+@RestController
+public class MessagesController {
+
+  @GetMapping("/messages")
+  public ResponseEntity<List<String>> messages () {
+    return ResponseEntity.ok(Arrays.asList("first", "second")); 
+  }
+  
+}
